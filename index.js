d22aef74a3cae843abee69841cae68f139a421ed
alchemist1111 <emoyoeugine@gmail.com>
2024-03-29 19:42:12 +0300

    adds index.html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+
+main.remove();
+
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Eugene is the champion";
